1bfdfee3bb1c2dcab4d29e8e0c228d081ba61d93
Mayme <maymehan@byu.edu>
2025-01-22 23:52:05 -0700

    Logic for bishop moves calculator. Later will add other pieces logic.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -9,8 +9,12 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessPiece {
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
 
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -29,15 +33,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -48,6 +51,17 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        PieceType pieceType = getPieceType();
+
+        PieceMovesCalculator calculator = new BishopMovesCalculator();
+
+        //use switch instead of if, so if bishop then call Bishopmovescalculator
+        switch (pieceType) {
+            case BISHOP:
+                return calculator.piecesMove(board, myPosition);
+            case KING:
+                return null;
+        }
+        return null;
     }
 }
--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -10,10 +10,42 @@ public abstract class PieceMovesCalculator {
 }
 
 class BishopMovesCalculator extends PieceMovesCalculator {
+    //so this inherets from piecemovescalcultor!!
 
     @Override
     public Collection<ChessMove> piecesMove(ChessBoard board, ChessPosition position) {
+        //create new ArrayList for valid moves
         Collection<ChessMove> moves = new ArrayList<>();
+
+        /*bishops can move diagonal ONLY*/
+        int[] x = {-1, -1, 1, 1};
+        int[] y = {-1, 1, -1, 1};
+
+        /*loop through 4 diagonal directions  */
+        for (int i = 0; i < 4; i++) {
+            int curr_x = position.getRow();
+            int curr_y = position.getColumn();
+            /*move diagonal until it hits the end of the board or hits another piece*/
+            while (true) {
+                curr_x += x[i];
+                curr_y += y[i];
+                /*end of board is anything less than 0 and greater than 7*/
+                if (curr_x < 0 || curr_x > 7 || curr_y < 0 || curr_y > 7) {
+                    break;
+                }
+
+                ChessPosition curr_position = new ChessPosition(curr_x, curr_y);
+                ChessPiece piece_curr_pos = board.getPiece(curr_position);
+                if (piece_curr_pos == null) {
+                    moves.add(new ChessMove(position, curr_position, null));
+                } else {
+                    if (piece_curr_pos.getTeamColor() != board.getPiece(position).getTeamColor()) {
+                        moves.add(new ChessMove(position, curr_position, null));
+                    }
+                    break;
+                }
+            }
+        }
         return moves;
     }
 }
