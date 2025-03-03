1b27ecfec78831745bec65424c42ea74bf8fc967
Mayme <maymehan@byu.edu>
2025-03-02 20:32:47 -0700

    Server files/directories

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,15 @@
 import chess.*;
+import spark.Spark;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+
+        //static files are located in /web
+        Spark.staticFiles.location("/web");
+        Spark.port(8080);
+        Spark.init();
+        System.out.println("Server running on port: 8080");
     }
 }
