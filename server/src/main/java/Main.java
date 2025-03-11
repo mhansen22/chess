e14819fd291f355c13f0c45007ef2e808f2597b8
Mayme <maymehan@byu.edu>
2025-03-10 23:45:11 -0600

    Main, minor update, added Server run

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,15 +1,12 @@
 import chess.*;
-import spark.Spark;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
 
-        //static files are located in /web
-        Spark.staticFiles.location("/web");
-        Spark.port(8080);
-        Spark.init();
-        System.out.println("Server running on port: 8080");
+        Server server = new Server();
+        server.run(8080);
     }
 }
