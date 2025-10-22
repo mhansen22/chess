8a3c7ddbb678f776acc6600e767bb16290c3029b
Mayme <maymehan@byu.edu>
2025-10-21 23:47:58 -0600

    userservice tweaks

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -6,46 +6,54 @@ import java.util.UUID;
 public class UserService {
     private AuthDAO auths;
     private UserDAO users;
+
     public UserService(UserDAO users, AuthDAO auths) {
         this.auths = auths;
         this.users = users;
     }
 
-    public AuthData register(String username, String password, String email) throws DataAccessException {
-        if (username == null || password == null || email == null){
-            throw new DataAccessException("bad requst");
-        }
-        //make new user
-        UserData newUser = new UserData(username, password, email);
-        users.createUser(newUser);
-        //this makes new rand token
-        String token = UUID.randomUUID().toString();
-        AuthData auth = new AuthData(token, username);
-        auths.createAuth(auth);
-        //now return the auth inf
-        return auth;
-    }
+    public record RegisterRequest(String username, String password, String email) {}
+    public record RegisterResult(String username, String authToken) {}
 
-    public AuthData login(String username, String password) throws DataAccessException {
-        if (username == null || password ==null) {
+    public RegisterResult register(RegisterRequest req) throws DataAccessException {
+        if (req == null || req.username() == null || req.password() == null || req.email() == null){
             throw new DataAccessException("bad request");
         }
-        var user = users.getUser(username);
-        if (user == null || (!user.password().equals(password))) {
+        //make a new user
+        UserData newUser = new UserData(req.username(), req.password(), req.email());
+        users.createUser(newUser);
+        //now mkae a new random token
+        String token = UUID.randomUUID().toString();
+        AuthData auth = new AuthData(token, req.username());
+        auths.createAuth(auth);
+        return new RegisterResult(req.username(), token);
+    }
+
+    public record LoginRequest(String username, String password) {}
+    public record LoginResult(String username, String authToken) {}
+
+    public LoginResult login(LoginRequest req) throws DataAccessException {
+        if (req == null || req.username() == null || req.password() ==null)
+            throw new DataAccessException("bad request");
+
+        var user = users.getUser(req.username());
+        if (user == null) {
             throw new DataAccessException("unauthorized");
         }
-        //this makes new token
+        if (!user.password().equals(req.password()))
+            throw new DataAccessException("unauthorized");
+        //make new token
         String token = UUID.randomUUID().toString();
-        AuthData auth = new AuthData(token, username);
-        auths.createAuth(auth);
-        return auth;
+        auths.createAuth(new AuthData(token, req.username()));
+
+        return new LoginResult(req.username(), token);
     }
 
     public void logout(String authToken) throws DataAccessException {
+        //i think just remove it??
         var auth = auths.getAuth(authToken);
-        if (auth== null) {
-            throw new DataAccessException("unauthorizd");
-        }
+        if (auth == null)
+            throw new DataAccessException("unauthorized");
         auths.deleteAuth(authToken);
     }
 }
