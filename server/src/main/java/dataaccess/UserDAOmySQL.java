269aa93a12416ec0ad1eeaba9c2c58f7dad6f5e0
Mayme <maymehan@byu.edu>
2025-11-03 21:16:14 -0700

    UserDAOmySQL cleaned up

--- server/src/main/java/dataaccess/UserDAOmySQL.java
+++ server/src/main/java/dataaccess/UserDAOmySQL.java
@@ -9,31 +9,13 @@ public class UserDAOmySQL implements UserDAO {
         configureTable();
     }
 
-    private final String createStatement =
-    """
-    CREATE TABLE IF NOT EXISTS users (
-        username VARCHAR(50) PRIMARY KEY,
-        password_hash VARCHAR(100) NOT NULL,
-        email VARCHAR(255),
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    ) ENGINE=InnoDB;
-    """;
-
-    private void configureTable() throws DataAccessException {
-        try (var conn = DatabaseManager.getConnection();
-             var ps = conn.prepareStatement(createStatement)) {
-            ps.executeUpdate();
-        } catch (SQLException ex) {
-            throw new DataAccessException("Unable to configure table (user)", ex);
-        }
-    }
-
     @Override
     public void clear() throws DataAccessException {
-        String clearingUsers = "DELETE FROM users";
-        try (var conn = DatabaseManager.getConnection();
-             var ps = conn.prepareStatement(clearingUsers)) {
-            ps.executeUpdate();
+        String statement = "DELETE FROM users";
+        try (var connection = DatabaseManager.getConnection()) {
+            try (var preparedStatement = connection.prepareStatement(statement)) {
+                preparedStatement.executeUpdate();
+            }
         } catch (SQLException e) {
             throw new DataAccessException("could not clear users", e);
         }
@@ -41,22 +23,23 @@ public class UserDAOmySQL implements UserDAO {
 
     @Override
     public void createUser(UserData data) throws DataAccessException {
-        if ((data.username() == null) ||(data.password() == null) || (data.email() == null)){
+        if ((data == null) || (data.username() == null) ||(data.password() == null) || (data.email() == null)){
             throw new DataAccessException("bad request");
         }
         //hash password here before entered into the database -->
         String hashedPassword = BCrypt.hashpw(data.password(), BCrypt.gensalt());
-        String insertingUser = "INSERT INTO users (username, password_hash, email) VALUES (?, ?, ?)";
+        String statement = "INSERT INTO users (username, password_hash, email) VALUES (?, ?, ?)";
 
-        try (var conn = DatabaseManager.getConnection();
-             var ps = conn.prepareStatement(insertingUser)) {
-            ps.setString(1, data.username());
-            //insert hashedPassword remember
-            ps.setString(2, hashedPassword);
-            ps.setString(3, data.email());
-            ps.executeUpdate();
+        try (var connection = DatabaseManager.getConnection()) {
+            try (var preparedStatement = connection.prepareStatement(statement)) {
+                preparedStatement.setString(1, data.username());
+                //insert hashedPassword remember
+                preparedStatement.setString(2, hashedPassword);
+                preparedStatement.setString(3, data.email());
+                preparedStatement.executeUpdate();
+            }
         } catch (java.sql.SQLIntegrityConstraintViolationException e) {
-            throw new DataAccessException("already  taken", e);
+            throw new DataAccessException("already taken", e);
         } catch (SQLException e) {
             throw new DataAccessException("could not insert user", e);
         }
@@ -67,21 +50,42 @@ public class UserDAOmySQL implements UserDAO {
         if (username ==null) {
             return null;
         }
-        String selectingUser = "SELECT username, password_hash, email FROM users WHERE username = ?";
-        try (var conn = DatabaseManager.getConnection();
-             var ps = conn.prepareStatement(selectingUser)) {
-            ps.setString(1, username);
-            try (var rs = ps.executeQuery()) {
-                if (rs.next()) {
-                    var userName = rs.getString("username");
-                    var hashedPassword = rs.getString("password_hash");
-                    var email = rs.getString("email");
-                    return new UserData(userName, hashedPassword, email);
+        String statement = "SELECT username, password_hash, email FROM users WHERE username = ?";
+        try (var connection = DatabaseManager.getConnection()) {
+            try (var preparedStatement = connection.prepareStatement(statement)) {
+                preparedStatement.setString(1, username);
+                try (var resultSet = preparedStatement.executeQuery()) {
+                    if (resultSet.next()) {
+                        var userName = resultSet.getString("username");
+                        var hashedPassword = resultSet.getString("password_hash");
+                        var email = resultSet.getString("email");
+                        return new UserData(userName, hashedPassword, email);
+                    }
                 }
             }
         } catch (SQLException e){
             throw new DataAccessException("could not get user", e);
         }
         return null;
+    }
+
+    private final String createStatement =
+            """
+            CREATE TABLE IF NOT EXISTS users (
+                username VARCHAR(50) PRIMARY KEY,
+                password_hash VARCHAR(100) NOT NULL,
+                email VARCHAR(255),
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            ) ENGINE=InnoDB;
+            """;
+
+    private void configureTable() throws DataAccessException {
+        try (var connection = DatabaseManager.getConnection()) {
+            try (var preparedStatement = connection.prepareStatement(createStatement)) {
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Unable to configure table (user)", ex);
+        }
     }
 }
