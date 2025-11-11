a8427c0661b16e0ea856c569ae09076f82ddb035
Mayme <maymehan@byu.edu>
2025-11-10 23:59:59 -0700

    test created

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,19 @@
 package client;
-
 import org.junit.jupiter.api.*;
 import server.Server;
-
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:" + port);
     }
 
     @AfterAll
@@ -24,6 +25,18 @@ public class ServerFacadeTests {
     @Test
     public void sampleTest() {
         Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void registerPositive() throws Exception {
+        String username = "mayme";
+        String password = "maymePassword";
+        String email = "maymehan@byu.edu";
+        var auth = facade.register(username, password, email);
+        //assert statements here::
+        assertNotNull(auth);
+        assertEquals(username, auth.username());
+        assertNotNull(auth.authToken());
     }
 
 }
